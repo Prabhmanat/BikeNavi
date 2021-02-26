dca9955e7c16b671648a52a25289fc03bed87f9f
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-02-25 20:09:51 -0500

    Package authentication Created

---
+++ app/src/main/java/com/example/bikenavi/authentication/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.bikenavi.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bikenavi.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
