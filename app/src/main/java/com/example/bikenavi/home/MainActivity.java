fc76939b1513873bfd4658f59c8e46bd9e533371
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-03-02 13:43:02 -0500

    Iteration 1 - Priyank

---
+++ app/src/main/java/com/example/bikenavi/home/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.bikenavi.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.bikenavi.R;
+import com.example.bikenavi.authentication.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // constants
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+}
