c375056eb6dab50873f2b8c077706176c73d4866
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-03-03 12:02:15 -0500

    Final modifications

--- app/src/main/java/com/example/bikenavi/authentication/LoginActivity.java
+++ app/src/main/java/com/example/bikenavi/authentication/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.bikenavi.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bikenavi.R;
 
@@ -12,5 +14,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onSignUpTextClick(View view) {
+        startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
     }
 }
